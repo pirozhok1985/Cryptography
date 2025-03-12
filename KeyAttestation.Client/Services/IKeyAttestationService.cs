6a4704e872f361980504108873df1660a99a51ee
anisimov evgenii <pirozhok1985@gmail.com>
2025-03-12 08:56:24 +0300

    Interface description

--- KeyAttestation.Client/Services/IKeyAttestationService.cs
+++ KeyAttestation.Client/Services/IKeyAttestationService.cs
@@ -5,7 +5,23 @@ namespace KeyAttestation.Client.Services;
 
 public interface IKeyAttestationService
 {
+    /// <summary>
+    /// Generates rsa keypair and certificate signing request
+    /// </summary>
+    /// <param name="saveAsPemEncodedFile">Indicates that request should be saved as pem encoded file</param>
+    /// <param name="fileName">The name of the file to be saved</param>
+    /// <param name="cancellationToken">Task canceling support</param>
+    /// <returns>Pkcs10GenerationResult:string? Csr, TpmKey? Ek, TpmKey? Aik</returns>
     public Task<Pksc10GenerationResult> GeneratePkcs10CertificationRequestAsync(bool saveAsPemEncodedFile, string? fileName, CancellationToken cancellationToken);
     
+    /// <summary>
+    /// Credential activation interface
+    /// </summary>
+    /// <param name="encryptedCredential">Credential to decrypt</param>
+    /// <param name="encryptedSecret">Encrypted seed used to calculate hmac and symmetric key</param>
+    /// <param name="ek">Endorsement key(TpmPublic)</param>
+    /// <param name="aik">Attestation identity key(TpmPublick)</param>
+    /// <param name="cancellationToken">Task canceling support</param>
+    /// <returns>CredentialActivationResult: byte[] ActivatedCredentials</returns>
     public Task<CredentialActivationResult> ActivateCredentialAsync(IdObject encryptedCredential, byte[] encryptedSecret, TpmKey ek, TpmKey aik, CancellationToken cancellationToken);
 }
--- KeyAttestation.Server/Services/IKeyAttestationService.cs
+++ KeyAttestation.Server/Services/IKeyAttestationService.cs
@@ -4,11 +4,33 @@ namespace KeyAttestation.Server.Services;
 
 public interface IKeyAttestationService
 {
+    /// <summary>
+    /// Retrieve AttestationData from Certificate Signing Request
+    /// </summary>
+    /// <param name="csr">Pem encoded certificate signing request</param>
+    /// <returns>AttestationData: Attest? Attestation, byte[]? Signature, TpmPublic? AikTpmPublic, TpmPublic? ClientTpmPublic, string? Csr</returns>
     public AttestationData GetAttestationDataAsync(string csr);
 
-    public Credendtial MakeCredentialsAsync(AttestationData data, byte[] ekPub);
+    /// <summary>
+    /// Make credential as a part of credential activation process(without tpm)
+    /// </summary>
+    /// <param name="data">Attestation data to process</param>
+    /// <param name="ekPub">Public portion of Endorsement key</param>
+    /// <returns>Credential: byte[] EncryptedIdentity, byte[] IntegrityHmac, byte[] EncryptedSecret, byte[] clearSecret, byte[] integrityHmac</returns>
+    public Credential MakeCredentialsAsync(AttestationData data, byte[] ekPub);
 
+    /// <summary>
+    /// Attestation statement validation
+    /// </summary>
+    /// <param name="data">Attestation data to process</param>
+    /// <returns>AttestationResult: bool Result, string? Message</returns>
     public AttestationResult AttestAsync(AttestationData data);
 
+    /// <summary>
+    /// Check whether received credential is equal to created one
+    /// </summary>
+    /// <param name="clientCredentials">Received credential</param>
+    /// <param name="serverCredentials">Source credential</param>
+    /// <returns>true if credentials are equal</returns>
     public bool CheckActivatedCredentials(byte[] clientCredentials, byte[] serverCredentials);
 }
