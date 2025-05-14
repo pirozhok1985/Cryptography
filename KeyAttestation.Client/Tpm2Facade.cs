903a75ab84728c101cb7659adb4ebd01a48a1767
Anisimov Evgenii <pirozhok1985@gmail.com>
2025-05-14 11:16:32 +0300

    tpm2Facade: fixed key generation error

--- KeyAttestation.Client/Tpm2Facade.cs
+++ KeyAttestation.Client/Tpm2Facade.cs
@@ -117,7 +117,7 @@ public sealed class Tpm2Facade<TTpm2Device>: ITpm2Facade
         switch (keyType)
         {
             case KeyType.Attestation:
-                typedAttributes = ObjectAttr.Restricted | ObjectAttr.Sign | ObjectAttr.SensitiveDataOrigin;
+                typedAttributes = ObjectAttr.Restricted | ObjectAttr.Sign;
                 keyParams = new RsaParms(new SymDefObject(), new SchemeRsassa(TpmAlgId.Sha256), 2048, 65537);
                 break;
             case KeyType.Ordinal:
@@ -127,7 +127,7 @@ public sealed class Tpm2Facade<TTpm2Device>: ITpm2Facade
         }
         
         var keyAttributes = ObjectAttr.FixedTPM | ObjectAttr.FixedParent |
-                            ObjectAttr.UserWithAuth | typedAttributes;
+                            ObjectAttr.UserWithAuth | ObjectAttr.SensitiveDataOrigin | typedAttributes;
         var keyTemplate = new TpmPublic(TpmAlgId.Sha256, keyAttributes, null, keyParams, new Tpm2bPublicKeyRsa());
 
         try
